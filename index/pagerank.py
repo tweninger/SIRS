a4f02afd0e1148127ea7317bbc5370d628cb6ede
tweninger <tweninge@nd.edu>
2019-05-07 14:45:39 -0400

    PageRank Code (only)

--- index/pagerank.py
+++ index/pagerank.py
@@ -1,6 +1,5 @@
 class Node(object):
-    def __init__(self, i):
-        self.id = i
+    def __init__(self):
         self.in_links = list()
         self.out_links = list()
         self.data = 1.0
@@ -26,6 +25,9 @@ class Node(object):
 
 class PageRank(object):
 
+    alpha = 0.85
+    iter = 50
+
     def __init__(self, webgraph, num_docs):
         """
         Initialize PageRank object, and loads the PageRank graph.
@@ -34,14 +36,33 @@ class PageRank(object):
         :param num_docs: Number of documents
         """
 
-        # IMPORTANT - Node ids should correspond to docids. (index starts at 1)
+        # IMPORTANT - Node ids should correspond to docids.
         self.graph = list()
         for n in range(1, num_docs+1):
-            self.graph.append(Node(n))
+            self.graph.append(Node())
+
+        print('Loading Web graph')
 
         # TODO - load graph from file. Create edges within Node object.
 
 
+        with open(webgraph, 'r') as r:
+            for line in r:
+                src, tgt = line.strip().split('->')
+                src = int(src)
+                tgt = int(tgt)
+                self.graph[src].add_out_link(tgt)
+                self.graph[tgt].add_in_link(src)
+
     def calculate_pagerank(self):
-        pass
+        print('Starting PageRank calculations')
+
         # TODO - fill in this function
+
+        for i in range(PageRank.iter):
+            for node in self.graph:
+                sum = 0.0
+                for inlink in node.get_in_links():
+                     sum += self.graph[inlink].get_data() / len(self.graph[inlink].get_out_links())
+                new_pr = (1-PageRank.alpha) + (PageRank.alpha * sum)
+                node.set_data(new_pr)
